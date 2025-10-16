ae83a4b8859ca6db0834c870423a5dfa5a8b1796
Oliver Benning <oliver.benning@snowflake.com>
2025-10-16 11:15:52 -0400

    Script

---
+++ si-setup.sql
@@ -0,0 +1,16 @@
+use role accountadmin;
+
+create or replace role snowflake_intelligence_admin;
+grant create warehouse on account to role snowflake_intelligence_admin;
+grant create database on account to role snowflake_intelligence_admin;
+grant create integration on account to role snowflake_intelligence_admin;
+
+set current_user = (select current_user());   
+grant role snowflake_intelligence_admin to user identifier($current_user);
+
+create database if not exists snowflake_intelligence;
+create schema if not exists snowflake_intelligence.agents;
+
+grant create agent on schema snowflake_intelligence.agents to role snowflake_intelligence_admin;
+
+ALTER ACCOUNT SET CORTEX_ENABLED_CROSS_REGION = 'ANY_REGION';
